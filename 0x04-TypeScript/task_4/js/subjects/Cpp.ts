c1ffeb2af4e32173b59b5b284aec673240e933fa
Ebere Onuobia <cynthiaonuobia@gmail.com>
2025-10-01 00:03:10 +0100

    Typescript fix

--- 0x04-TypeScript/task_4/js/subjects/Cpp.ts
+++ 0x04-TypeScript/task_4/js/subjects/Cpp.ts
@@ -1,19 +1,25 @@
-// task_4/js/subjects/Cpp.ts
-
-/// <reference path="./Subject.ts" />
-
-namespace Subjects {
+export namespace Subjects {
   export interface Teacher {
+    firstName: string;
     experienceTeachingC?: number;
   }
 
+  // Add a base Subject class
+  export class Subject {
+    teacher?: Teacher;
+  }
+
+  // Cpp extends Subject, not Subjects
   export class Cpp extends Subject {
     getRequirements(): string {
       return "Here is the list of requirements for Cpp";
     }
 
     getAvailableTeacher(): string {
-      if (this.teacher?.experienceTeachingC > 0) {
+      if (
+        this.teacher?.experienceTeachingC &&
+        this.teacher.experienceTeachingC > 0
+      ) {
         return `Available Teacher: ${this.teacher.firstName}`;
       }
       return "No available teacher";
--- 0x04-TypeScript/task_4/js/subjects/Java.ts
+++ 0x04-TypeScript/task_4/js/subjects/Java.ts
@@ -1,22 +1,20 @@
-// task_4/js/subjects/Java.ts
-
+// <reference path="./Teacher.ts" />
 /// <reference path="./Subject.ts" />
 
 namespace Subjects {
-  export interface Teacher {
-    experienceTeachingJava?: number;
-  }
-
-  export class Java extends Subject {
-    getRequirements(): string {
-      return "Here is the list of requirements for Java";
+    export interface Teacher {
+        experienceTeachingJava?: number;
     }
 
-    getAvailableTeacher(): string {
-      if (this.teacher?.experienceTeachingJava > 0) {
-        return `Available Teacher: ${this.teacher.firstName}`;
-      }
-      return "No available teacher";
+    export class Java extends Subject {
+        getRequirements(): string {
+            return "Here is the list of requirements for Java";
+        }
+        getAvailableTeacher(): string {
+            if(!this.teacher.experienceTeachingJava || this.teacher.experienceTeachingJava === 0) {
+                return "No available teacher";
+            } else {
+                return `Available Teacher: ${this.teacher.firstName}`;
+            }
+        }
     }
-  }
-}
